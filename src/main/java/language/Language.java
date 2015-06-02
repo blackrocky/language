dc5c804cd004c37fa0e28db3891f6c71a3b2a934
blackrocky <robbie.martinus@yahoo.com.au>
2015-06-02 21:34:29 +1000

    Initial files

---
+++ src/main/java/language/Language.java
@@ -0,0 +1,10 @@
+package language;
+
+import org.joda.time.LocalTime;
+
+public class Language {
+	public static void main(String[] args) {
+		LocalTime localTime = new LocalTime();
+		System.out.println("Hello world: " + localTime);
+	}
+}
