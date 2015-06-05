0b31f0cc22c695634cb7b086d0696d8a4f4bf1b5
d642852 <robbie.martinus@team.telstra.com>
2015-06-05 16:42:22 +1000

    refactorin

--- src/main/java/language/Language.java
+++ src/main/java/language/Language.java
@@ -10,22 +10,24 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.util.*;
 
+import static language.Dictionary.*;
+
 @Component
 public class Language {
-	private static final Logger LOGGER = LoggerFactory.getLogger(Language.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Language.class);
 
     private static final String UNKNOWN = "UNKNOWN";
     private FileReader fileReader;
-	private Dictionary dictionary;
+    private Dictionary dictionary;
 
     public Language() {
     }
 
     @Autowired
-	public Language(FileReader fileReader, Dictionary dictionary) {
+    public Language(FileReader fileReader, Dictionary dictionary) {
         this.fileReader = fileReader;
-		this.dictionary = dictionary;
-	}
+        this.dictionary = dictionary;
+    }
 
     public Dictionary getDictionary() {
         return dictionary;
@@ -44,54 +46,57 @@ public class Language {
         return determineLanguage(pathStr, dictionaryFiles);
     }
 
-	public String determineLanguage(String pathStr, List<File> dictionaryFiles) throws IOException {
-		File inputFile = null;
-		try {
-			inputFile = fileReader.readAllLinesWithCharacterCheck(pathStr);
-		} catch (FileNotValidException e) {
-			return UNKNOWN;
-		}
-
-		Map<String, Integer> languageScore = new HashMap<>();
-        for (File dictionaryFile : dictionaryFiles) {
-            languageScore.put(dictionaryFile.getLanguage(), 0);
+    public String determineLanguage(String pathStr, List<File> dictionaryFiles) throws IOException {
+        File inputFile;
+        try {
+            inputFile = fileReader.readAllLinesWithCharacterCheck(pathStr);
+        } catch (FileNotValidException e) {
+            return UNKNOWN;
         }
 
-		for (String line : inputFile.getLines()) {
-			LOGGER.debug("line = {}", line);
+        Map<String, Integer> languageScore = calculateScore(dictionaryFiles, inputFile);
 
-			Map<String, Set<String>> dictionaryMap = dictionary.getDictionary();
-			for (Map.Entry<String, Set<String>> entry : dictionaryMap.entrySet()) {
-				LOGGER.debug("Checking {}", entry.getKey());
-				Set<String> dictionaryWords = entry.getValue();
-
-				line = StringUtils.lowerCase(line.replaceAll(Dictionary.NON_ALPHABET_AND_SPACE_REGEX, ""));
-				String[] wordArray = StringUtils.split(line);
-				Set<String> words = new HashSet<>(Arrays.asList(wordArray));
-
-				if (words != null) {
-					for (String word : words) {
-						if (dictionaryWords.contains(word)) {
-							languageScore.put(entry.getKey(), languageScore.get(entry.getKey()) + 1);
-						}
-					}
-				}
-			}
-		}
-		LOGGER.debug("languageScore: {}", languageScore);
-
-		Map.Entry<String, Integer> maxEntry = null;
+        Map.Entry<String, Integer> maxEntry = null;
         int totalScore = 0;
-		for (Map.Entry<String, Integer> scoreEntry : languageScore.entrySet()) {
-			if (maxEntry == null || scoreEntry.getValue() > maxEntry.getValue()) {
-				maxEntry = scoreEntry;
-			}
+        for (Map.Entry<String, Integer> scoreEntry : languageScore.entrySet()) {
+            if (maxEntry == null || scoreEntry.getValue() > maxEntry.getValue()) {
+                maxEntry = scoreEntry;
+            }
             totalScore += scoreEntry.getValue();
-		}
+        }
 
         if (maxEntry == null || totalScore == 0) {
             return UNKNOWN;
         }
-		return maxEntry.getKey();
-	}
+        return maxEntry.getKey();
+    }
+
+    private Map<String, Integer> calculateScore(List<File> dictionaryFiles, File inputFile) {
+        Map<String, Integer> languageScore = new HashMap<>();
+        for (File dictionaryFile : dictionaryFiles) {
+            languageScore.put(dictionaryFile.getLanguage(), 0);
+        }
+
+        for (String line : inputFile.getLines()) {
+            LOGGER.debug("line = {}", line);
+
+            Map<String, Set<String>> dictionaryMap = dictionary.getDictionary();
+            for (Map.Entry<String, Set<String>> entry : dictionaryMap.entrySet()) {
+                LOGGER.debug("Checking {}", entry.getKey());
+                Set<String> dictionaryWords = entry.getValue();
+
+                line = StringUtils.lowerCase(line.replaceAll(NON_ALPHABET_AND_SPACE_REGEX, ""));
+                String[] wordArray = StringUtils.split(line);
+                Set<String> words = new HashSet<>(Arrays.asList(wordArray));
+
+                for (String word : words) {
+                    if (dictionaryWords.contains(word)) {
+                        languageScore.put(entry.getKey(), languageScore.get(entry.getKey()) + 1);
+                    }
+                }
+            }
+        }
+        LOGGER.debug("languageScore: {}", languageScore);
+        return languageScore;
+    }
 }
