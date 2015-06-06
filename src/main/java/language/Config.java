f3e783579f95f79110a8010dde68864bfcd9f0e9
blackrocky <robbie.martinus@yahoo.com.au>
2015-06-06 17:08:10 +1000

    updated configuration

--- src/main/java/language/Config.java
+++ src/main/java/language/Config.java
@@ -1,12 +1,13 @@
 package language;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
-@org.springframework.context.annotation.Configuration
+@Configuration
 @PropertySource("classpath:language.properties")
-public class Configuration {
+public class Config {
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertyConfig() {
         return new PropertySourcesPlaceholderConfigurer();
