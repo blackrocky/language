1a1bf96cee2fd47bb7d0a292bc25617291d66481
blackrocky <robbie.martinus@yahoo.com.au>
2015-06-08 09:19:55 +1000

    use lambda and fixed bug

--- src/test/java/language/LanguageMockTest.java
+++ src/test/java/language/LanguageMockTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
+import java.util.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -61,5 +62,33 @@ public class LanguageMockTest {
         assertThat(language.getTextFileName(), is("mock text file name"));
         assertThat(language.getTextFileFolder(), is("mock text file folder"));
         assertThat(language.getDictionary(), is(dictionary));
+    }
+
+    @Test
+    public void should_calculate_correct_scores_given_duplicates_in_text_file() {
+        List<String> spanishLines = Arrays.asList("ola", "como", "esta");
+        List<String> englishLines = Arrays.asList("hi", "how", "are", "you");
+        List<String> frenchLines = Arrays.asList("bonjour", "cava");
+
+        List<File> dictionaryFiles = new ArrayList<>();
+        File spanishFile = new File(spanishLines, "mock parent dictionary", "spanish");
+        File englishFile = new File(englishLines, "mock parent dictionary", "eNglIsh");
+        File frenchFile = new File(frenchLines, "mock parent dictionary", "French");
+        dictionaryFiles.add(spanishFile);
+        dictionaryFiles.add(englishFile);
+        dictionaryFiles.add(frenchFile);
+
+        Map<String, Set<String>> myDictionary = new HashMap<>();
+        myDictionary.put("SPANISH", new HashSet<>(spanishLines));
+        myDictionary.put("ENGLISH", new HashSet<>(englishLines));
+        myDictionary.put("FRENCH", new HashSet<>(frenchLines));
+
+        when(language.getDictionary().getDictionary()).thenReturn(myDictionary);
+
+        File inputFile = new File(Arrays.asList("como", "esta", "como", "esta"), "mock parent", "TEXT");
+        Map<String, Integer> score = language.calculateScore(dictionaryFiles, inputFile);
+        assertThat(score.get("SPANISH"), is(2));
+        assertThat(score.get("ENGLISH"), is(0));
+        assertThat(score.get("FRENCH"), is(0));
     }
 }
